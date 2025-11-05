db2d36af8cad8257f35b2a4e16f1820d624774ff
agx <agxr@genocide.fun>
2025-11-06 00:16:16 +0500

    Test cube added

---
+++ prefabs/test_cube.scm
@@ -0,0 +1,10 @@
+(prefab
+  (name "test_cube")
+  (description "Test cube for rendering")
+  
+  (component "shape"
+    (type "box")
+    (position 0 2 0)
+    (dimensions 1.0 1.0 1.0)
+    (color 0.8 0.3 0.3 1.0)
+    (visible #t)))
